4ca43e741bbaf6e2898f31baecf2e508f14a1624
Jay Graves <jay@skabber.com>
2011-12-09 11:35:41 -0700

    Remove the DE testing credentials.

--- unoffical-twitter-sdk/OAuth/OAuthConsumerCredentials.h
+++ unoffical-twitter-sdk/OAuth/OAuthConsumerCredentials.h
@@ -1,4 +1,4 @@
 #error Use your Twitter keys from developer.twitter.com
 
-#define kDEConsumerKey @"njXqKBprrm9m3eYB9hSyoA"
-#define kDEConsumerSecret @"4pWLLFoTb8U0fkYOr8LAOVWCtTH8PqSAeYmT5yfDfRs"
+#define kDEConsumerKey @"GetYourOwn"
+#define kDEConsumerSecret @"GetYourOwn"
